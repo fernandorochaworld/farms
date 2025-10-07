4ad39d0cdc00c2470ee7c76e7beb38f1be7fda18
Fernando Rocha <fernandorochaworld@gmail.com>
2025-10-07 00:16:46 -0700

    New: Added Make files

---
+++ Makefile
@@ -0,0 +1,6 @@
+# Main Makefile for Flutter Farms Project
+
+# Include Flutter commands from makefiles folder
+include makefiles/flutter.mk
+
+.DEFAULT_GOAL := help
---
+++ makefiles/flutter.mk
@@ -0,0 +1,64 @@
+# Flutter Development Commands
+
+.PHONY: install setup run clean build test help
+
+# Install Flutter dependencies
+install:
+	@echo "Installing Flutter dependencies..."
+	flutter pub get
+
+# Setup the project (install dependencies and generate code if needed)
+setup: install
+	@echo "Setting up the project..."
+	@echo "Setup complete!"
+
+# Run the Flutter application
+run:
+	@echo "Running Flutter application..."
+	flutter run
+
+# Run on specific device
+run-chrome:
+	flutter run -d chrome
+
+run-windows:
+	flutter run -d windows
+
+run-android:
+	flutter run -d android
+
+# Clean build artifacts
+clean:
+	@echo "Cleaning build artifacts..."
+	flutter clean
+
+# Build the application
+build:
+	@echo "Building Flutter application..."
+	flutter build apk
+
+build-windows:
+	flutter build windows
+
+build-web:
+	flutter build web
+
+# Run tests
+test:
+	@echo "Running tests..."
+	flutter test
+
+# Help command
+help:
+	@echo "Available commands:"
+	@echo "  make install       - Install Flutter dependencies"
+	@echo "  make setup         - Setup the project"
+	@echo "  make run           - Run the Flutter application"
+	@echo "  make run-chrome    - Run on Chrome"
+	@echo "  make run-windows   - Run on Windows"
+	@echo "  make run-android   - Run on Android"
+	@echo "  make clean         - Clean build artifacts"
+	@echo "  make build         - Build APK"
+	@echo "  make build-windows - Build for Windows"
+	@echo "  make build-web     - Build for Web"
+	@echo "  make test          - Run tests"
